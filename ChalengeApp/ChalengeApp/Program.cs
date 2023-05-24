0bdfeba5d33d331aa379a5c511a668442d29e60a
Kamil-Bien <1kamilbien1@gmail.com>
2023-05-24 19:28:21 +0200

    Praca domowa dzień 4

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,4 +1,21 @@
-﻿Console.WriteLine("Co jest czerwone i szkodzi na zęby?");
-Console.WriteLine("Cegła!");
-Console.WriteLine("Badum cssssss");
-Console.WriteLine("Kurtyna");
+﻿string name = "Rysio";
+char sex = 'M'; //M = Male
+                //F = Female
+int age = 33;
+
+if (sex == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && sex == 'M')
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("To tylko Rysio");
+}

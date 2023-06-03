22167fa57c7dbbb8d3460e008f4e3825329f5be8
Kamil-Bien <1kamilbien1@gmail.com>
2023-06-04 01:08:15 +0200

    Homework day 6th

---
+++ ChalengeApp/ChalengeApp/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace ChalengeApp
+{
+    public class Employee
+    {
+        public string name { get; private set; }
+        public string surname { get; private set; }
+        public int age { get; private set; }
+        private List<int> score = new List<int>();
+        
+
+        public Employee(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+
+        }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+         this.score.Add(number);      
+        }
+    }
+}
--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,23 +1,75 @@
-﻿int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-int[] countNumber = new int[10];
+﻿using ChalengeApp;
+using System.Threading.Tasks.Sources;
 
+Employee worker1 = new Employee("Jan", "Dzban", 24);
+Employee worker2 = new Employee("Roman", "Bosy", 35);
+Employee worker3 = new Employee("Żylisław", "Świerczyński-Brzęczyszczykiewicz", 55);
 
+worker1.AddScore(5);
+worker1.AddScore(4);
+worker1.AddScore(5);
+worker1.AddScore(4);
+worker1.AddScore(5);
 
-foreach (var oneNumber  in letters)
-{	
-    for (int i = 0; i < 10; i++)
-	{
-		string iString = i.ToString();
-        string oneNumberString = oneNumber.ToString();
-        if (iString == oneNumberString)
-		{
-			countNumber[i]++;
-		}
-	}
-}
-for (int i = 0; i < 10; i++)
+worker2.AddScore(5);
+worker2.AddScore(2);
+worker2.AddScore(4);
+worker2.AddScore(5);
+worker2.AddScore(4);
+
+worker3.AddScore(5);
+worker3.AddScore(6);
+worker3.AddScore(2);
+worker3.AddScore(5);
+worker3.AddScore(5);
+
+List<Employee> workers = new List<Employee>()
 {
-    Console.WriteLine(i + " => " + countNumber[i]);
+    worker1, worker2, worker3
+};
+List<Employee> employeeOfTheMonth = new List<Employee>();
+
+
+
+
+int bestScore = -1;
+Employee bestWorker = null;
+
+
+
+foreach (var worker in workers)
+{
+    if (worker.Result > bestScore)
+    {
+        bestScore = worker.Result;
+        bestWorker = worker;
+    }
+}
+
+
+foreach (var worker in workers)
+{
+    if (worker.Result == bestWorker.Result)
+    {
+        bestWorker = worker;
+        employeeOfTheMonth.Add(bestWorker);
+    }
+}
+
+if (employeeOfTheMonth.Count == 1)
+{
+    Console.WriteLine("Pracownik miesiąca:");
+    Console.WriteLine();
+    Console.WriteLine(bestWorker.name + " " + bestWorker.surname + ", wiek: " + bestWorker.age + " lat, uzyskał wynik: " + bestWorker.Result + "pkt");
+}
+else
+{
+    Console.WriteLine("Pracownicy miesiąca:");
+
+    foreach (var bestWorkers in employeeOfTheMonth)
+    {
+        Console.WriteLine();
+        Console.WriteLine(bestWorkers.name + " " + bestWorkers.surname + ", wiek: " + bestWorkers.age + " lat, uzyskał wynik: " + bestWorkers.Result + "pkt");
+        Console.WriteLine();
+    }
 }
